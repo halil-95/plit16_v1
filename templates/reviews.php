733bcd31fb35cfc6a8bb10b020ae0cdd08994116
root <root@codertime.site.local>
2024-03-23 23:42:01 +0000

    reviews remove element and change for

--- templates/reviews.php
+++ templates/reviews.php
@@ -4,14 +4,9 @@
             <h2>ОТЗЫВЫ О НАС</h2>
         </div>
         <div class="reviews__images">
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
-            <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
+            <?php for($i = 0; $i < 8; $i++):?>
+                <div class="reviews__image"><img src="dist/image/2.jpg" title="some img" alt="some text"></div>
+            <?php endfor;?>
         </div>
     </div>
 </section>
