bfdcc670258e4fa8ef83146888b25b49e3abfde2
root <root@codertime.site.local>
2024-03-23 23:51:03 +0000

    add micro component for header

--- templates/header.php
+++ templates/header.php
@@ -19,33 +19,18 @@
         codertime.site created on the basis of another site</title>
 </head>
 <body>
-    <?php 
-    /**
-     * variable path
-     */
-    $dir = "templates/part/contact/"; 
-    ?>
-<header class="block_header flex-row">
-    <div class="header_bg" style="background-image: url('/dist/image/bg_result.jpg')"></div>
-    <div class="header container flex-row container">
-        <div class="block_header__left flex-row">
-            <div class="header__logo flex-row">
-                <?php include( $dir . "logo.php" );?>
+    <?php $dir = "templates/part/contact/"; //variable path ?>
+
+    <header class="block_header flex-row">
+        <div class="header_bg" style="background-image: url('/dist/image/bg_result.jpg')"></div>
+        <div class="header container flex-row container">
+            <div class="block_header__left flex-row">
+                    <?php include( $dir . "logo.php" );?>
+                    <?php include( $dir . "address.php" );?> 
             </div>
-            <div class="header__address flex-column">
-                <?php include( $dir . "address.php" );?> 
-            </div>
-        </div>
-        <div class="block_header__right flex-row">
-            <div class="header__phone flex-column">
+            <div class="block_header__right flex-row">
                 <?php include( $dir . "phone.php" );?> 
-            </div>
-            <div class="header__callback callback flex-row">
-                <div class="callback__callme"><button class="button"  data-fancybox data-src="#call-me">Перезвонить мне</button></div>
-                <div class="callback__price">
-                    <button class="button"><a href="#price">Скачать прайс-листы</a></button>
-                </div>
+                <?php include( $dir . "button.php" );?> 
             </div>
         </div>
-    </div>
-</header>
+    </header>
---
+++ templates/part/contact/button.php
@@ -0,0 +1,6 @@
+<div class="header__callback callback flex-row">
+    <div class="callback__callme"><button class="button"  data-fancybox data-src="#call-me">Перезвонить мне</button></div>
+    <div class="callback__price">
+        <button class="button"><a href="#price">Скачать прайс-листы</a></button>
+    </div>
+</div>
