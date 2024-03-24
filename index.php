2438de10d302d6a78ffceb3852b64a0c275f5483
root <root@codertime.site.local>
2024-03-24 00:09:27 +0000

    inside inclide change variable

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <?php include("templates/part/form.php");//form section?>
 <main class="main">
     <?php $name_files = array("hero","benefit","offer","map","price","order","reviews","contact","order","map","copy");?>
-    <?php foreach ($name_files as $file):?>
-        <?php include("templates/".$file.".php");//hero section?>
+    <?php foreach ($name_files as $file): $files = "templates/".$file.".php";?>
+        <?php include($files);//hero section?>
     <?php endforeach;?>
 </main>
 <?php include("templates/footer.php");//footer section?>
